69972f5d7053c523bfd7d87dd20c05c49d72cf17
matej-safaric <matej.safaric@gmail.com>
2024-09-24 13:46:20 +0200

    Prepared B-matrix functions for separate boundary conditions

--- 2D_QWE.py
+++ 2D_QWE.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+#==============================================================================#
+# GRID FUNCTIONS
+#==============================================================================#
 def grid(n : int):
     '''Returns a square grid of n ** 2 points.'''
     out = n * []
@@ -10,17 +13,35 @@ def grid(n : int):
     return out
 
 
-def edges(grid):
-    '''Returns the edges of a grid.'''
+def edgesNeumann(grid):
+    '''Returns the edges of a grid according to the Neumann
+    boundary conditions.'''
     n = len(grid)
     out = []
     for i in range(n):
         for j in range(n):
-            if i < n - 1:
-                out.append((grid[i][j], grid[i + 1][j]))
             if j < n - 1:
                 out.append((grid[i][j], grid[i][j + 1]))
+            if i < n - 1:
+                out.append((grid[i][j], grid[i + 1][j]))
     return out
+
+def edgesDirichlet(grid):
+    '''Returns the edges of a grid according to the Dirichlet
+    boundary conditions.'''
+    n = len(grid)
+    out = []
+    for i in range(n):
+        for j in range(n):
+            # If the point is on the boundary, add a self-loop.
+            if i in [0, n - 1] or j in [0, n - 1]:
+                out.append((grid[i][j], grid[i][j]))
+            if j < n - 1:
+                out.append((grid[i][j], grid[i][j + 1]))
+            if i < n - 1:
+                out.append((grid[i][j], grid[i + 1][j]))    
+    return out
+                
 
 def conc(grid):
     '''Returns the concatenation of the points in a grid.'''
@@ -30,10 +51,35 @@ def conc(grid):
             out.append(point)
     return out
 
-def B(n : int):
-    '''Returns the B matrix for a grid of n ** 2 points.'''
+
+
+
+#==============================================================================#
+#                           MATRIX FUNCTIONS                                   #
+#==============================================================================#
+def B_Dirichlet(n : int):
+    '''Returns the B matrix for a grid of n ** 2 points according to 
+    the Dirichlet boundary condition.'''
     g = grid(n)
-    e = edges(g)
+    e = edgesDirichlet(g)
+    g = conc(g)
+    out = np.zeros((len(e), len(g)))
+    for i, (a, b) in enumerate(e):
+        if a == b:
+            if a[0] in [0, n - 1] and a[1] in [0, n - 1]:
+                out[i][g.index(a)] = np.sqrt(2) # Corner boundary points
+            else:
+                out[i][g.index(a)] = 1  # Non-corner boundary points
+        else:
+            out[i][g.index(a)] = 1
+            out[i][g.index(b)] = -1
+    return out.transpose()
+
+def B_Neumann(n : int):
+    '''Returns the B matrix for a grid of n ** 2 points according to
+    the Neumann boundary condition.'''
+    g = grid(n)
+    e = edgesNeumann(g)
     g = conc(g)
     out = np.zeros((len(e), len(g)))
     for i, (a, b) in enumerate(e):
@@ -41,7 +87,13 @@ def B(n : int):
         out[i][g.index(b)] = -1
     return out.transpose()
 
-print(B(2))
+print(B_Dirichlet(2))
 
 # TODO: Sort out the order of the points in the grid.
 
+# Hamiltonian setup
+from Hamiltonians.Libraries import HamiltonianEvolution as HE
+
+n = 3
+H = HE.BHamiltonian2D(n, B_Dirichlet(n), 'Dirichlet')
+print(H)
