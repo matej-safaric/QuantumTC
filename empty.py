cf69a3b6108391541c1d91fa56fdf890a51dcf76
matej-safaric <matej.safaric@gmail.com>
2024-10-07 11:34:34 +0200

    Experimented with 3d plots with non-square grids

--- empty.py
+++ empty.py
@@ -13,9 +13,34 @@ a = 0.01
 #                                   Main                                       #
 #------------------------------------------------------------------------------#
 
-for i in range(n):
+def f1():
+    for i in range(n):
+        print('#==============================================================================#')
+        print('h = ', n * a * np.sqrt(3) / 2)
+        print('l = ', (2*n + 1) * a / 2)
+        print('Difference: ', (2*n + 1) * a / 2 - n * a * np.sqrt(3) / 2)
     print('#==============================================================================#')
-    print('h = ', n * a * np.sqrt(3) / 2)
-    print('l = ', (2*n + 1) * a / 2)
-    print('Difference: ', (2*n + 1) * a / 2 - n * a * np.sqrt(3) / 2)
-print('#==============================================================================#')
+
+
+
+
+#==============================================================================#
+#                                   3D Plots                                   #
+#------------------------------------------------------------------------------#
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+data = np.random.rand(100, 3)
+
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111, projection="3d")
+
+x = np.array(11 * list(range(11)))
+y = np.array([11 * [i] for i in range(11)]).flatten()
+from Hamiltonians.Libraries import HamiltonianEvolution as HE
+z = np.array([HE.gaussian2D(i, j, 5, 5, 3) for i,j in zip(x,y)])
+
+print(data)
+
+ax.plot_trisurf(x,y,z)
+plt.show()
